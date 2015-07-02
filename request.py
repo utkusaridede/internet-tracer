585a201cc76609f11818d0cfd9acbab1426ee59e
utkudoganay <utkudoganay@hotmail.com>
2015-07-02 16:53:08 +0300

    Channel listen code

--- request.py
+++ request.py
@@ -2,7 +2,7 @@ import json
 import urllib2
 
 #req = urllib2.Request("http://www.macvendorlookup.com/api/v2/10:bf:48:1c:94:55")
-req = urllib2.Request("http://www.macvendorlookup.com/api/v2/b8:b4:2e:c4:4a:21")
+req = urllib2.Request("http://www.macvendorlookup.com/api/v2/18:cf:5e:90:4b:f7")
 opener = urllib2.build_opener()
 f = opener.open(req)
 json = json.loads(f.read())
