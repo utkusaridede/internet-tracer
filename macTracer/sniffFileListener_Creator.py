183753219b493d89673c37d51312905b746f1a0c
utkudoganay <utkudoganay@hotmail.com>
2015-07-09 13:12:16 +0300

    sniff database listener(server)

---
+++ macTracer/sniffFileListener_Creator.py
@@ -0,0 +1,15 @@
+import sqlite3
+import os
+
+
+while 1:
+	fo = open("sniffFileCount.txt","r")
+	count = fo.read()
+	count = int(count)
+	fo.close()
+	os.system("nc -l -p 8001 -q 1 > /home/utku/summerseed/internetTracer/sniffer/data/sniff%d.sqlite" % count)
+	count = count + 1
+	fr = open("sniffFileCount.txt","w")
+	fr.write(str(count))
+	fr.close()
+
