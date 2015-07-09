c94045d6d43d380f8eff622421a5f125ad25d1bc
utkusaridede <utkusaridede@gmail.com>
2015-07-10 02:47:17 +0300

    Havayi dinle button update

---
+++ daemon/individuaListener.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ifconfig wlan0 down
+iwconfig wlan0 mode monitor
+ifconfig wlan0 up
+iwconfig wlan0 channel 7
+timeout 10 tcpdump -i wlan0 -n -s 64 -w ../sniffer/input/interAir.pcap
+ifconfig wlan0 down
+iwconfig wlan0 mode managed
+ifconfig wlan0 up
+dhclient wlan0
+cd ../sniffer
+python air_visualizer.pyc input/interAir.pcap
+cd ../macTracer
+python companyCounter.py
